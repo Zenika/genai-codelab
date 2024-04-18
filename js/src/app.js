b7d240f2c5367db702fa23714564fd4100a00252
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-04-18 20:14:27 +0200

    :arrow_up: Migrate source code to ESM

--- js/src/app.js
+++ js/src/app.js
@@ -1,20 +1,20 @@
-const { ChatOllama }  = require("@langchain/community/chat_models/ollama");
-const { MessagesPlaceholder, PromptTemplate, ChatPromptTemplate, FewShotChatMessagePromptTemplate } = require("@langchain/core/prompts");
-const { ChatMessageHistory } = require("langchain/stores/message/in_memory");
-const { RunnableWithMessageHistory } = require("@langchain/core/runnables");
-const { loadSummarizationChain } = require("langchain/chains");
-const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
-const fs = require("fs");
+import { ChatOllama } from '@langchain/community/chat_models/ollama';
+import {
+  MessagesPlaceholder,
+  PromptTemplate,
+  ChatPromptTemplate,
+  FewShotChatMessagePromptTemplate,
+} from '@langchain/core/prompts';
+import { ChatMessageHistory } from 'langchain/stores/message/in_memory';
+import { RunnableWithMessageHistory } from '@langchain/core/runnables';
+import { loadSummarizationChain } from 'langchain/chains';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { readFile } from 'node:fs/promises';
 
-const OLLAMA_URL = 'http://localhost:11434'
-const MODEL = 'openhermes'
-const main = async () => {
-
-  const llm = new ChatOllama({
-    baseUrl: OLLAMA_URL,
-    model: MODEL,
-    temperature: 0.1
-  });
-};
-
-main();
+const OLLAMA_URL = 'http://localhost:11434';
+const MODEL = 'openhermes';
+const llm = new ChatOllama({
+  baseUrl: OLLAMA_URL,
+  model: MODEL,
+  temperature: 0.1,
+});
