ae9dd3550f6249956887f8d5ebaf22f49af10c80
glefloch <glfloch@gmail.com>
2024-04-18 11:49:52 +0200

    publish java and javscript correction

--- js/src/app.js
+++ js/src/app.js
@@ -16,8 +16,84 @@ const main = async () => {
     temperature: 0.1
   });
 
-  // YOUR CODE GOES HERE
+  // First Step
+  const prompt = 'Who are you ?'
+  const result = await llm.invoke(prompt);
+  console.log(result.content);
 
+  // Prompt Template
+  const template = PromptTemplate.fromTemplate('explain me this Regular expression{regexp}')
+  const chain = template.pipe(llm)
+  const regexpExplanation = await chain.invoke({regexp: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'});
+  console.log(regexpExplanation.content)
+
+  // Few Short learning
+  const examples = [
+      {animal: 'cow', sound: 'moo'},
+      {animal: 'cat', sound: 'meow'},
+      {animal: 'dog', sound: 'woof'}
+  ]
+  const examplePrompt = ChatPromptTemplate.fromTemplate(`Human: {animal} 
+  AI: {sound}`)
+
+  const fewShotPrompt = new FewShotChatMessagePromptTemplate({
+    prefix: "You are an animal expert able to guess the sound an animal does based on a name.",
+    suffix: "Human: {input}",
+    examplePrompt,
+    examples,
+    inputVariables: ["input"],
+  });
+
+  const formattedPrompt = await fewShotPrompt.format({input: 'lion'})
+  const response = await llm.invoke(formattedPrompt)
+  console.log(response.content)
+
+  // Memory
+  const runnableWithMessageHistoryPrompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You are a helpful assistant. Answer all questions to the best of your ability.",
+    ],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{input}"],
+  ]);
+    
+  const chain2 = runnableWithMessageHistoryPrompt.pipe(llm);
+  const chatHistory = new ChatMessageHistory();
+
+  const chainWithMessageHistory = new RunnableWithMessageHistory({
+    runnable: chain2,
+    getMessageHistory: (_sessionId) => chatHistory,
+    inputMessagesKey: "input",
+    historyMessagesKey: "chat_history",})
+
+
+  const r = await chainWithMessageHistory.invoke(
+    {
+      input:
+        "Translate this sentence from English to French: I love programming.",
+    },
+    { configurable: { sessionId: "unused" } }
+  )
+
+  console.log(r.content)
+    const secondR = await chainWithMessageHistory.invoke(
+    {
+      input:
+        "What Did I just ask you ?",
+    },
+    { configurable: { sessionId: "unused" } }
+  );
+
+  // Summarize
+  const summarizationChain = loadSummarizationChain(llm, { type: "map_reduce" });
+  const text = fs.readFileSync("java_introduction.txt", "utf8");
+  
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
+  const summary = await summarizationChain.invoke({input_documents: docs});
+
+  console.log(summary.text)
 };
 
 main();
